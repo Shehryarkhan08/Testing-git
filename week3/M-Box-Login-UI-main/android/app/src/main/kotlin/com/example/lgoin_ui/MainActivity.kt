81d536d315cc432e984610c49cacf4da5e1b97e7
Shehryarkhan08 <shary.boss66@gmail.com>
2023-05-05 10:57:55 -0700

    week4/5/6/7

--- week3/M-Box-Login-UI-main/android/app/src/main/kotlin/com/example/lgoin_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lgoin_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- week3ui/fa20bcs062/android/app/src/main/kotlin/com/example/fa20bcs062/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fa20bcs062
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
